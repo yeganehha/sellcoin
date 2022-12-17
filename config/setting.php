b5d5e133ca4546b37882a12d5dfc10787cb9514e
Erfan Ebrahimi <persionhost@gmail.com>
2022-12-17 22:23:07 +0330

    use .env file in setting

--- config/setting.php
+++ config/setting.php
@@ -3,11 +3,11 @@
 return [
     'coins' => [
         'list' => [
-            'cache_time' => 5 * 60 // time of caching list of coins per each exchange platform
+            'cache_time' => env('COINS_LIST_CACHE' , 5 * 60) // time of caching list of coins per each exchange platform
         ],
-        'cache_time' => 60 // time of caching information of special coin
+        'cache_time' => env('COIN_CACHE' , 60) // time of caching information of special coin
     ],
     'purchase' => [
-        'wait_for_confirm' => 5 // minute wait before auto cancel  purchase
+        'wait_for_confirm' => env('PURCHASE_WAIT_FOR_CONFIRM' , 5) // minute wait before auto cancel  purchase
     ]
 ];
